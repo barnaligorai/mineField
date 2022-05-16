656279ab906440d00d9dc797911228b06fc8fb76
Barnali Gorai <99495284+barnaligorai@users.noreply.github.com>
2022-05-16 12:04:30 +0530

    Changed code in start_game.sh.

--- start_game.sh
+++ start_game.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 echo '{"path":[3,13,23,33,34,35,36,46,56,57,67,77,87,86,96],"row":10,"col":10, "pos":3}' > mineField.json
-
 node startGame.js 3
+
+# echo '{"path":[2,5,6,9,6],"row":3,"col":3, "pos":2}' > mineField.json
+# node startGame.js 2
 
 game_state=$(grep 'Over' mineField.json)
 
